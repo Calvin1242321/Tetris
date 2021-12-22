fd8ebd58824a93e55b9fe193bc0d6a98366a1286
Calvin Cheng <80262794+Calvin1242321@users.noreply.github.com>
2021-12-22 14:52:11 +0800

    last updated Dec/22/2021
    fixed some bug occurs while rotating and made a queue to store tetrominos

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,25 +1,65 @@
 
 #include "Headers/Tetris.h"
-
 #include <iostream>			// for testing, after finished this class you should delete this line.
 
 Tetris::Tetris()
 {
 	for (int i = 0;i < HEIGHT;i++)
 		for (int j = 0;j < WIDTH;j++)	field[i][j] = -1;
+	int x = 0;
+
+	srand(time(NULL));
+	for (int i = 0;i < PREVIEW_NUM;i++)
+	{
+		x = rand() % 7;
+		tetromino.push(x);
+	}
+	
 }
 
 Tetris::~Tetris()
 {
 }
 
+int Tetris::popAndSet(pos a[])
+{
+	int x = tetromino.front();
+	tetromino.pop();
+	switch (x)
+	{
+	case 0:
+		set_assist(shape_J, a);
+		break;
+	case 1:
+		set_assist(shape_S, a);
+		break;
+	case 2:
+		set_assist(shape_Z, a);
+		break;
+	case 3:
+		set_assist(shape_O, a);
+		break;
+	case 4:
+		set_assist(shape_T, a);
+		break;
+	case 5:
+		set_assist(shape_I, a);
+		break;
+	case 6:
+		set_assist(shape_L, a);
+		break;
+	default:
+		break;
+	}
+	return x;
+}
 
 //	last modified:	Dec/19/2021
 //	Creator:		Calvin1242321
 //	function:		Pick a shape randomly then return that shape.
 //	lack:			1. It should follow the rules of tetris design.
 //					2. Using queue to preview next 4 shapes. 
-int Tetris::setup_shape(pos a[], int* color)
+void Tetris::setup_shape(pos a[], int* color)
 {
 	// reset
 	j = 0;
@@ -29,37 +69,39 @@ int Tetris::setup_shape(pos a[], int* color)
 
 	srand(time(NULL));
 
-	int choise = rand() % 7;		// Pick a num at random as the first shape.
-	*color = rand() % 4;
-	
-	switch (choise)
+	// Pick a num at random as the first shape.
+	int choice = rand() % 7;
+	tetromino.push(choice);
+	*color = tetromino.front();
+	tetromino.pop();
+	//*color = choice;
+	switch (*color)
 	{
 	case 0:
-		set_assist(shape_I, a);
-		break;
-	case 1:
-		set_assist(shape_O, a);
-		break;
-	case 2:
-		set_assist(shape_L, a);
-		break;
-	case 3:
 		set_assist(shape_J, a);
 		break;
-	case 4:
+	case 1:
 		set_assist(shape_S, a);
 		break;
-	case 5:
+	case 2:
 		set_assist(shape_Z, a);
 		break;
-	case 6:
+	case 3:
+		set_assist(shape_O, a);
+		break;
+	case 4:
 		set_assist(shape_T, a);
+		break;
+	case 5:
+		set_assist(shape_I, a);
+		break;
+	case 6:
+		set_assist(shape_L, a);
 		break;
 	default:
 		break;
 	}
-	
-	return choise;
+
 }
 void Tetris::set_assist(int shape[][4], pos a[])
 {
@@ -78,25 +120,25 @@ void Tetris::rotate(pos a[], int index)
 	switch (index)
 	{
 	case 0:
-		rotate_assist(I_FORMNUM, shape_I, a);
-		break;
-	case 1:
-		rotate_assist(O_FORMNUM, shape_O, a);
-		break;
-	case 2:
-		rotate_assist(L_FORMNUM, shape_L, a);
-		break;
-	case 3:
 		rotate_assist(J_FORMNUM, shape_J, a);
 		break;
-	case 4:
+	case 1:
 		rotate_assist(S_FORMNUM, shape_S, a);
 		break;
-	case 5:
+	case 2:
 		rotate_assist(Z_FORMNUM, shape_Z, a);
 		break;
-	case 6:
+	case 3:
+		rotate_assist(O_FORMNUM, shape_O, a);
+		break;
+	case 4:
 		rotate_assist(T_FORMNUM, shape_T, a);
+		break;
+	case 5:
+		rotate_assist(I_FORMNUM, shape_I, a);
+		break;
+	case 6:
+		rotate_assist(L_FORMNUM, shape_L, a);
 		break;
 	default:
 		break;
@@ -104,8 +146,8 @@ void Tetris::rotate(pos a[], int index)
 }
 void Tetris::rotate_assist(int s_Mxn, int shape[][4], pos a[])
 {
-	if (j != s_Mxn - 1)	j++;
-	else j = 0;
+	if (j != s_Mxn - 1)	j++;		//	cycle 0 ~ FORMNUM
+	else j = 0;						//
 
 	pos t[4];
 	for (int i = 0;i < 4;i++)	t[i] = a[i];
@@ -131,12 +173,15 @@ void Tetris::move(pos a[], char dir)
 	int k = 0;
 	if (dir == 'r')	k = 1;
 	else if (dir == 'l') k = -1;
-	counter_move += k;
 	
 	for (int i = 0;i < 4;i++)
 		t[i].x += k;
 	if (check_availble(t))
+	{
 		for (int i = 0;i < 4;i++)	a[i] = t[i];
+		counter_move += k;
+	}
+		
 }
 
 //	creator:	Calvin1242321	Dec/19/2021
@@ -174,13 +219,14 @@ void Tetris::drop_ins(pos a[], int color)
 	{
 		field[a[i].y][a[i].x] = color;
 	}
+	clean();
 }
 
 
 //	last modified:	DEC/19/2021
 //	Creator:		Calvin1242321
 //	status:			It's working.
-void Tetris::falling(pos a[])
+bool Tetris::falling(pos a[], int color)
 {
 	pos t[4];
 	for (int i = 0;i < 4;i++)	t[i] = a[i];
@@ -191,6 +237,14 @@ void Tetris::falling(pos a[])
 		for (int i = 0;i < 4;i++)	a[i] = t[i];
 		counter_fall++;
 	}
+	else
+	{
+		for (int i = 0;i < 4; i++)
+			field[a[i].y][a[i].x] = color;
+		clean();
+		return true;
+	}
+	return false;
 }
 
 
@@ -212,5 +266,35 @@ void Tetris::show_locate(pos a[], int color)
 	for (int i = 0;i < 4;i++)
 	{
 		//	abcde[t[i].y][t[i].x] = color;				abcde must be clean when every loop is started.
+	}
+}
+
+void Tetris::clean()
+{
+	for (int i = HEIGHT - 1; i >= 0;i--)
+	{
+		bool sc = true;
+		for (int j = 0;j < WIDTH;j++)
+		{
+			if (field[i][j] == -1)
+			{
+				sc = false;
+				break;
+			}	
+		}
+		if (sc)
+		{
+			clean_assist(i);
+			i = HEIGHT;
+		}
+	}
+}
+void Tetris::clean_assist(int i )
+{
+	if (i != 0)
+	{
+		for (int j = 0;j < WIDTH;j++)
+			field[i][j] = field[i - 1][j];
+		clean_assist(i - 1);
 	}
 }
